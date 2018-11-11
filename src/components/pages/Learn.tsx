ac46b4e82c5a33c628e35eafd19fe0d1c2737131
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2018-11-11 19:38:19 +0100

    wip learn

--- src/components/pages/Learn.tsx
+++ src/components/pages/Learn.tsx
@@ -13,7 +13,7 @@ type Props = RouteComponentProps;
 
 function Learn(_props: Props) {
   const colorInput = useInput("#A2D");
-  const hue = (mapToHue(colorInput.value) || { name: "unknown" }).name;
+  const hue = mapToHue(colorInput.value) || { name: "unknown" };
   const lightness = mapToLightness(colorInput.value);
   const saturation = mapToSaturation(colorInput.value);
 
@@ -24,30 +24,25 @@ function Learn(_props: Props) {
       <div className={styles.container}>
         <ColorInput className={styles.colorInput} autoFocus {...colorInput} pattern={VALID_HEX_COLOR_PATTERN} />
 
-        <ColorChart color={colorInput.value} />
-
-        <span>Hue: {hue}</span>
-        <span>Saturation: {saturation}</span>
-        <span>Lightness: {lightness}</span>
-        <hr />
-
-        <ColorWheel />
-      </div>
-
-      <div>
-        <h2>Step 1: Use the 3-digit shorthand</h2>
-      </div>
-      <div>
-        <h2>Step 2: Build a mental chart</h2>
-      </div>
-      <div>
-        <h2>Step 3: Get the hue from the shape</h2>
-      </div>
-      <div>
-        <h2>Step 4: Get the light from the total</h2>
-      </div>
-      <div>
-        <h2>Step 5: Get the saturation from the range</h2>
+        <div>
+          <h2>Step 1: Use the 3-digit shorthand</h2>
+        </div>
+        <div>
+          <h2>Step 2: Build a mental chart</h2>
+          <ColorChart color={colorInput.value} />
+        </div>
+        <div>
+          <h2>Step 3: Get the hue from the shape</h2>
+          <ColorWheel selectedColor={hue.name} />
+        </div>
+        <div>
+          <h2>Step 4: Get the light from the total</h2>
+          <span>Lightness: {lightness}</span>
+        </div>
+        <div>
+          <h2>Step 5: Get the saturation from the range</h2>
+          <span>Saturation: {saturation}</span>
+        </div>
       </div>
     </div>
   );
