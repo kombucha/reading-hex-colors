704a3a61024a7d64a7268ceb7c0634c709026652
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2018-11-11 14:33:47 +0100

    tweak ColorChart

--- src/components/ColorChart.tsx
+++ src/components/ColorChart.tsx
@@ -5,13 +5,14 @@ import { parseHexColor, asPercentage } from "../utils";
 
 interface Props {
   color: string;
+  size?: number;
 }
 
-function ColorChart({ color }: Props) {
+function ColorChart({ color, size = 100 }: Props) {
   const colorRgbComponents = parseHexColor(color);
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ width: size, height: size }}>
       {colorRgbComponents.map((val, idx) => (
         <div className={styles.bar} key={idx}>
           <div className={styles.value} style={{ height: asPercentage(val) }} />
