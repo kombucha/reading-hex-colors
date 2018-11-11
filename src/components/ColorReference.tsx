da108fb339415b0c19bfb5642ebc382c70c038cb
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2018-11-11 15:30:05 +0100

    Now with legend

--- src/components/ColorReference.tsx
+++ src/components/ColorReference.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { COLOR_MAP } from "../utils";
 import ColorChart from "./ColorChart";
@@ -8,28 +8,38 @@ interface Props {
   size?: number;
 }
 
-function ColorReference({ size = 500 }: Props) {
+function ColorReference({ size = 400 }: Props) {
+  const [viewColorName, setViewedColorName] = useState("");
+
   const chartSize = size / 6;
   const r = (size - chartSize) / 2;
   const offset = r;
   const thetaOffset = -Math.PI / 2;
   const thetaSlices = (2 * Math.PI) / COLOR_MAP.length;
 
-  return (
-    <ul className={styles.list} style={{ width: size, height: size }}>
-      {COLOR_MAP.map((c, idx) => {
-        const theta = thetaOffset + idx * thetaSlices;
-        const top = offset + r * Math.sin(theta);
-        const left = offset + r * Math.cos(theta);
-        const style = { top, left };
+  const containerStyle = { width: size, height: size };
 
-        return (
-          <li className={styles.item} key={c.value} style={style}>
-            <ColorChart color={c.value} size={chartSize} />
-          </li>
-        );
-      })}
-    </ul>
+  return (
+    <div className={styles.wrapper} style={containerStyle}>
+      <ul className={styles.list} style={containerStyle}>
+        {COLOR_MAP.map((color, idx) => {
+          const theta = thetaOffset + idx * thetaSlices;
+          const top = offset + r * Math.sin(theta);
+          const left = offset + r * Math.cos(theta);
+          const style = { top, left, width: chartSize, height: chartSize };
+
+          const setColor = () => setViewedColorName(color.name);
+          const unsetColor = () => setViewedColorName("");
+
+          return (
+            <li className={styles.item} key={color.value} style={style} onMouseOver={setColor} onMouseOut={unsetColor}>
+              <ColorChart color={color.value} size={chartSize} />
+            </li>
+          );
+        })}
+      </ul>
+      {viewColorName && <span className={styles.colorLabel}>{viewColorName}</span>}
+    </div>
   );
 }
 
