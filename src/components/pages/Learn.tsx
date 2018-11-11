13c3567d5daf3f2d92890b1349b4dee619ba43ab
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2018-11-11 22:34:11 +0100

    Fixes

--- src/components/pages/Learn.tsx
+++ src/components/pages/Learn.tsx
@@ -37,9 +37,9 @@ function Learn(_props: Props) {
           description={
             <>
               <p>
-                The goal of this exercise is to learn how to read hex color and have a good enough approximation mental
-                representation. Hex color representation is meant to be understood by computers, but it is possible to
-                build an intuition around it. The content of this page is based on the awesome talk{" "}
+                The goal of this exercise is to learn how to read hex color and have a good enough mental approximation.
+                Hex color representation is meant to be understood by computers 🤖, but it is possible to build an
+                intuition around it. The content of this page is based on the awesome talk{" "}
                 <ExternalLink href="https://desandro.com">David DeSandro</ExternalLink> gave at dotCSS 2018.
               </p>
               <p>Let's start !</p>
