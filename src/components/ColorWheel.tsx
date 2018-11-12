e53f6486dcabc04767b27ae7b8824626e72b8ff8
Vincent Lemeunier <vincent.lemeunier@algolia.com>
2018-11-12 23:09:32 +0100

    Dynamic ColorWheel size <3

--- src/components/ColorWheel.tsx
+++ src/components/ColorWheel.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import useComponentSize, { ComponentSize } from "@rehooks/component-size";
 import cn from "classnames";
 
 import { COLOR_MAP } from "../utils";
@@ -6,24 +7,27 @@ import ColorChart from "./ColorChart";
 import styles from "./ColorWheel.module.css";
 
 interface Props {
-  size?: number;
+  minSize?: number;
+  maxSize?: number;
   selectedColor?: string;
 }
 
-function ColorWheel({ size = 400, selectedColor = "" }: Props) {
+function ColorWheel({ minSize = 300, maxSize = 600, selectedColor = "" }: Props) {
   const [hoveredColor, setHoveredColor] = useState("");
+  const ref = useRef(null);
+  const { width: computedSize = minSize } = (useComponentSize as (ref: any) => ComponentSize)(ref);
 
-  const chartSize = Math.round(size / 6);
-  const r = (size - chartSize) / 2;
+  const chartSize = Math.round(computedSize / 6);
+  const r = (computedSize - chartSize) / 2;
   const offset = r;
   const thetaOffset = -Math.PI / 2;
   const thetaSlices = (2 * Math.PI) / COLOR_MAP.length;
 
-  const containerStyle = { width: size, height: size };
+  const containerStyle = { width: computedSize, height: computedSize };
   const highlightedColor = hoveredColor || selectedColor;
 
   return (
-    <div className={styles.wrapper} style={containerStyle}>
+    <div className={styles.wrapper} style={{ height: computedSize, maxWidth: maxSize }} ref={ref}>
       <ul className={styles.list} style={containerStyle}>
         {COLOR_MAP.map((color, idx) => {
           const theta = thetaOffset + idx * thetaSlices;
