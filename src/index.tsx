8b6524d400c403d622604f374811c2abb8bb4e45
Vincent Lemeunier <vincent.lemeunier@algolia.com>
2018-11-14 13:14:18 +0100

    use polyfill correctly

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import "resize-observer-polyfill";
+import ResizeObserver from "resize-observer-polyfill";
+
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -6,6 +7,8 @@ import App from "./components/App";
 
 import "reset-css";
 import "./base.css";
+
+(window as any).ResizeObserver = ResizeObserver;
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
