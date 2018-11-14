baed69bc5757adb612275ccc5087f873d0938329
Vincent Lemeunier <vincent.lemeunier@algolia.com>
2018-11-14 23:49:27 +0100

    Add some credits

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,10 @@ import ExternalLink from "./base/ExternalLink";
 const Footer: React.SFC = () => (
   <footer className={styles.container}>
     <div className={styles.content}>
-      <div>Made with colorlove by Vincent Lemeunier</div>
+      <div>
+        Made with colorlove by Vincent Lemeunier, with help from{" "}
+        <ExternalLink href="https://twitter.com/auareyou">Aurora</ExternalLink>
+      </div>
       <ul className={styles.links}>
         <li>
           <ExternalLink href="http://github.com/kombucha">Github</ExternalLink>
