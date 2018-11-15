26e69baa2cb8b3b060c2255d991aada6e825a82c
Vincent Lemeunier <vincent.lemeunier@algolia.com>
2018-11-15 20:41:25 +0100

    Change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,15 +7,12 @@ const Footer: React.SFC = () => (
   <footer className={styles.container}>
     <div className={styles.content}>
       <div>
-        Made with colorlove by Vincent Lemeunier, with help from{" "}
-        <ExternalLink href="https://twitter.com/auareyou">Aurora</ExternalLink>
+        Made with colorlove by <ExternalLink href="https://twitter.com/auareyou">Vincent Lemeunier</ExternalLink>, with
+        help from <ExternalLink href="https://twitter.com/auareyou">Aurora</ExternalLink>
       </div>
       <ul className={styles.links}>
         <li>
-          <ExternalLink href="http://github.com/kombucha">Github</ExternalLink>
-        </li>
-        <li>
-          <ExternalLink href="http://twitter.com/mrkombu">Twitter</ExternalLink>
+          <ExternalLink href="http://github.com/kombucha/reading-hex-colors">Github</ExternalLink>
         </li>
       </ul>
     </div>
